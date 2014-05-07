e742418fc76c378d114edfc28e3d4fe6df279603
Tom Readings <digitisatom@gmail.com>
2014-05-07 13:43:12 +0100

    test to predict content of page using capybara gem

---
+++ spec/requests/tasks_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe "Tasks" do
+  describe "GET /tasks" do
+    it "display some tasks" do
+      # use capybara to visit route
+      visit tasks_path
+      page.should have_content 'go to bed'
+      
+    end
+  end
+end
